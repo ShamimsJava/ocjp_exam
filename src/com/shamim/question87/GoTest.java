628e86aeaa77fe99ee6548e566425abe0607157f
shamimsjava <shamimsjava@gmail.com>
2018-04-17 23:04:57 +0600

    practice up to 87

--- src/com/shamim/question87/GoTest.java
+++ src/com/shamim/question87/GoTest.java
@@ -1,4 +1,38 @@
 package com.shamim.question87;
 
 public class GoTest {
+
+    public static void main(String[] args) {
+        Sente a = new Sente();
+        a.go();
+
+        Goban b = new Goban();
+        b.go();
+
+        Stone c = new Stone();
+        c.go();
+    }
+}
+//--------------------------------------------------------------------
+class Sente implements Go{
+
+    @Override
+    public void go() {
+        System.out.println("go in Sente. ");
+    }
+}
+//---------------------------------------------------------------------
+class Goban extends Sente{
+    @Override
+    public void go() {
+        System.out.println("go in Goban");
+    }
+}
+//---------------------------------------------------------------------
+class Stone extends Goban implements Go{
+
+}
+//---------------------------------------------------------------------
+interface  Go{
+    public void go();
 }
